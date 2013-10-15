72a7202f7699ede7f683d8028c97faca807aa627
Nikola Kantar <nkantar@gmail.com>
2013-10-14 20:54:57 -0700

    Updates to is_prime

--- nik_math.rb
+++ nik_math.rb
@@ -1,13 +1,32 @@
 #/usr/bin/ruby
 
 def is_prime(number)
-  divisor = 2 
+
+  ############
+  # exceptions
+
+  # negative, 0, 1 even
+  if number < 2 || (number % 2 == 0 && number != 2)
+    return false
+  end
+
+  # 2
+  if number == 2
+    return true
+  end
+
+
+  ###########
+  # iteration
+
+  divisor = 3
   root = Math.sqrt(number).to_i
+
   while divisor <= root do
     if number % divisor == 0
       return false
     end 
-    divisor += 1
+    divisor += 2
   end 
   return true
 end
